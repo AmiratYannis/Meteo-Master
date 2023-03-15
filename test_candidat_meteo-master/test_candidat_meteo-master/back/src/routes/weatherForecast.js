26b1c6b37a50f1707d7021449bd23fe555cac1b3
AmiratYannis <46192264+AmiratYannis@users.noreply.github.com>
2023-03-15 16:47:36 +0100

    update url of api rest for weather forecast

--- test_candidat_meteo-master/test_candidat_meteo-master/back/src/routes/weatherForecast.js
+++ test_candidat_meteo-master/test_candidat_meteo-master/back/src/routes/weatherForecast.js
@@ -4,9 +4,9 @@ const router= express.Router()
 const weatherUtils=require('../weatherUtils')
 
 router.get('/:citieInsee',(req,res)=>{
-    const cityId=req.params.cityInsee
+    const cityInsee=req.params.cityInsee
 
-    axios.get('https://api.meteo-concept.com/api/ephemeride/0?token=281861bc3e43474217eff22b4bcd406433694b182034e09399554888c0ec5c35')
+    axios.get(`https://api.meteo-concept.com/api/ephemeride/0?token=ce3f9e80a67aafb9c8f02ad0328714a2591c79244122b41695d61cbdea7c345f&insee=${cityInsee}`)
         .then(res=>{
             const weather=res.data
             console.log(res.data)
